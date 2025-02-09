b6c41c1ccabc76d3cb6eaaac17792c18556dc90f
Daniel Whiffing <danielwhiffing@gmail.com>
2025-02-08 20:16:33 -0500

    Removes extra redirect

--- next.config.mjs
+++ next.config.mjs
@@ -5,13 +5,7 @@ const nextConfig = {
     source: '/',
     destination: '/games',
     permanent: true
-  },
-  {
-    source: '/:path*',
-    has: [{ type: 'header', key: 'host', value: 'directive.software' }],
-    destination: 'https://www.directive.software/:path*',
-    permanent: true,
-  },]
+  }]
 };
 
 export default nextConfig;
